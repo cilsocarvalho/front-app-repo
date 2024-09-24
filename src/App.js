e1e155140ac998505fcfe3f870701209fecb524b
Cilso <cilso.carvalho@gmail.com>
2024-09-24 20:50:09 +0000

    Add new blog version

--- src/App.js
+++ src/App.js
@@ -64,7 +64,19 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog
+            return (
+    <div className={classes.root}>
+      <AppBar position="static" style={{ background: '#2E3B55' }}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <CloudIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            align="center"
+            className={classes.title}
+          >
+            EKS DEMO Blog version 1
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
